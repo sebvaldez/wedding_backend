63ddbb49346cb24918f97331c01392ff1a98032a
Seb <valdez.sebastian4@gmail.com>
2023-07-31 23:19:18 -0700

    include groupId, createdAt, updatedAt

--- layers/services/nodejs/memberDTO.js
+++ layers/services/nodejs/memberDTO.js
@@ -1,21 +1,35 @@
+// memberDTO.js
+
 class MemberDTO {
   static transform(member) {
     const id = member.PK && typeof member.PK === 'string' && member.PK.includes('#')
       ? member.PK.split('#')[1]
       : null;
 
-      return {
-          id: id,
-          phoneNumber: member.phoneNumber,
-          emailedInvitation: member.emailedInvitation,
-          dinnerSelection: member.dinnerSelection,
-          lastName: member.lastName,
-          email: member.email,
-          firstName: member.firstName,
-          plannedTransportation: member.plannedTransportation,
-          specialSippingpreference: member.specialSippingpreference,
-          checkIn: member.checkIn
-      };
+    let groupId = member.SK && typeof member.SK === 'string' && member.SK.includes('GROUP#')
+      ? member.SK.split('#')[1]
+      : null;
+
+    // Set groupId to null if it's 'NO_GROUP'
+    if (groupId === 'NO_GROUP') {
+      groupId = null;
+    }
+
+    return {
+        id: id,
+        phoneNumber: member.phoneNumber,
+        emailedInvitation: member.emailedInvitation,
+        dinnerSelection: member.dinnerSelection,
+        lastName: member.lastName,
+        email: member.email,
+        firstName: member.firstName,
+        plannedTransportation: member.plannedTransportation,
+        specialSippingpreference: member.specialSippingpreference,
+        checkIn: member.checkIn,
+        groupId: groupId,
+        createdAt: member.createdAt,
+        updatedAt: member.updatedAt
+    };
   }
 
   static transformList(members) {
